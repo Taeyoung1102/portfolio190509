5049061350a8902d957af68fa8c5d8b5ebb34132
Taeyoung1102 <43396049+Taeyoung1102@users.noreply.github.com>
2019-05-09 16:40:09 +0900

    Delete cursor.js

--- js/cursor.js
+++
@@ -1,7 +0,0 @@
-var cursor = document.getElementById('cursor');
-document.addEventListener('mousemove', function(e){
-    let x = e.clientX;
-    let y = e.clientY;
-    cursor.style.left = x + "px";
-    cursor.style.top = y + "px";
-});
